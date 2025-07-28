fda14eaab563366688e212c9ab8dbd32fcc9c158
168501 <victor.muriuki@strathmore.edu>
2025-07-28 13:54:19 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+    require 'config/dbConnect.php';
+    require 'includes/header.php';
+    require 'includes/nav.php';
+?>
+
+<div class="header">
+    <h1>About Us</h1>
+</div>
+<div class="row">
+    <div class="content">
+        <h2>Welcome to Our About Page</h2>
+        <p><img src="images/teacher.webp" alt="Our Team">Here you can learn more about our mission, vision, and the team that makes it all happen. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?Here you can learn more about our mission, vision, and the team that makes it all happen. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
+Sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                    <h2>Learn More About Our Team and Mission</h2>
+
+        <p>sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+    </div>
+    <div class="sidebar">
+                <h2>Side Bar</h2>
+        <p>We are a team of dedicated professionals committed to delivering high-quality services and products.</p>
+        <p>This is the about page. It contains information about the website, its purpose, and the team behind it. 
+        You can find details on our mission, vision, and values here. We aim to provide a comprehensive overview of 
+        our services and how we can help you achieve your goals.</p>
+    </div>
+</div>
+<?php
+    require 'includes/footer.php';
+?>
---
+++ products.php
@@ -0,0 +1,67 @@
+<?php
+    require 'config/dbConnect.php';
+    require 'includes/header.php';
+    require 'includes/nav.php';
+?>
+
+<div class="row">
+    <div class="content">
+<h1>Heading 1</h1>
+
+   <div>
+       <img src="images/clock.jpg" alt="Wall Clock" width="200" height="200">
+   </div> 
+
+<table>
+    <caption>Stationery Products</caption>
+    <tr>
+        <th>SN</th>
+        <th>Items</th>
+        <th>Quantity</th>
+        <th>U Price</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Pen</td>
+        <td>10</td>
+        <td>$1.00</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Pencil</td>
+        <td>20</td>
+        <td>$0.50</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Notebook</td>
+        <td>15</td>
+        <td>$2.00   </td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Ruler</td>
+        <td>25</td>
+        <td>$1.50</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Pencil</td>
+        <td>20</td>
+        <td>$0.50</td>
+    </tr>
+</table>
+
+
+    </div>
+    <div class="sidebar">
+                <h2>Side Bar</h2>
+        <p>We are a team of dedicated professionals committed to delivering high-quality services and products.</p>
+        <p>This is the about page. It contains information about the website, its purpose, and the team behind it. 
+        You can find details on our mission, vision, and values here. We aim to provide a comprehensive overview of 
+        our services and how we can help you achieve your goals.</p>
+    </div>
+</div>
+<?php
+    require 'includes/footer.php';
+?>
